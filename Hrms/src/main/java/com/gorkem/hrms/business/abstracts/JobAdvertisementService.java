b513cd8c4595fad1213ea9355af31b0c40d96e0e
Gorkemblm <gorkemblm@gmail.com>
2021-05-29 06:55:14 +0300

    29.05.2021 06:55 Add JobAdvertisementService

--- Hrms/src/main/java/com/gorkem/hrms/business/abstracts/JobAdvertisementService.java
+++ Hrms/src/main/java/com/gorkem/hrms/business/abstracts/JobAdvertisementService.java
@@ -1,9 +1,25 @@
 package com.gorkem.hrms.business.abstracts;
 
+import com.gorkem.hrms.core.utilities.results.DataResult;
+import com.gorkem.hrms.core.utilities.results.Result;
 import com.gorkem.hrms.entities.concretes.JobAdvertisement;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface JobAdvertisementService {
-    List<JobAdvertisement> getAll();
+
+    DataResult<List<JobAdvertisement>> getAll();
+
+    Result add(JobAdvertisement jobAdvertisement);
+
+    DataResult<List<JobAdvertisement>> findByIsActiveTrue();
+
+    DataResult<List<JobAdvertisement>> findByUpdatedAtAndIsActiveTrue(LocalDate localDate);
+
+    DataResult<List<JobAdvertisement>> findByEmployerId(int id);
+
+    DataResult<JobAdvertisement> updateStatusJobAdvertisement(int id, boolean status);
+
+    DataResult<List<JobAdvertisement>> findByEmployer_CompanyName(String companyName);
 }
