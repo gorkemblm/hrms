684cc251f64540af7000286ebf798b5d30af3ee5
Gorkemblm <gorkemblm@gmail.com>
2021-06-06 15:44:45 +0300

    add fake check service

--- Hrms/src/main/java/com/gorkem/hrms/core/adapters/mernis/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.gorkem.hrms.core.adapters.mernis;
-
-import com.gorkem.hrms.entities.abstracts.IndividualUser;
-
-public interface AuthenticationService {
-
-    boolean isRealPerson(IndividualUser individualUser);
-}
