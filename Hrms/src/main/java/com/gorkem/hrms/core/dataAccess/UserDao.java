8f8c56552f88d56e7e756791ae0741960117a263
Gorkemblm <gorkemblm@gmail.com>
2021-06-07 14:53:20 +0300

    add user crud operations  and map

--- Hrms/src/main/java/com/gorkem/hrms/core/dataAccess/UserDao.java
+++ Hrms/src/main/java/com/gorkem/hrms/core/dataAccess/UserDao.java
@@ -4,4 +4,7 @@ import com.gorkem.hrms.core.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+
+    User findById(int id);
+    User findByEmailAndPasswordHash(String email, String password);
 }
