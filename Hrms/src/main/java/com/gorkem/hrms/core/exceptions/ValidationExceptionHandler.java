4477f8a1d5a27d40e4046e1bb8459ade84d93e01
Gorkemblm <gorkemblm@gmail.com>
2021-06-07 14:53:37 +0300

    add global exception handler

---
+++ Hrms/src/main/java/com/gorkem/hrms/core/exceptions/ValidationExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gorkem.hrms.core.exceptions;
+
+import com.gorkem.hrms.core.utilities.results.ErrorDataResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ErrorDataResult<Object> errors = new ErrorDataResult<>("Validation Errors!", validationErrors);
+        return errors;
+    }
+}
