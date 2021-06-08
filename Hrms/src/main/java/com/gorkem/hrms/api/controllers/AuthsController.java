f21d2020c53633ad9c9264e1867a55586baeffb8
Gorkemblm <gorkemblm@gmail.com>
2021-06-08 22:46:02 +0300

    deleted result operations

--- Hrms/src/main/java/com/gorkem/hrms/api/controllers/AuthsController.java
+++ Hrms/src/main/java/com/gorkem/hrms/api/controllers/AuthsController.java
@@ -1,8 +1,6 @@
 package com.gorkem.hrms.api.controllers;
 
 import com.gorkem.hrms.business.abstracts.AuthService;
-import com.gorkem.hrms.business.constants.Messages;
-import com.gorkem.hrms.core.utilities.results.ErrorResult;
 import com.gorkem.hrms.core.utilities.results.Result;
 import com.gorkem.hrms.entities.dtos.authDtos.EmployerForRegisterDto;
 import com.gorkem.hrms.entities.dtos.authDtos.JobSeekerForRegisterDto;
@@ -29,31 +27,18 @@ public class AuthsController {
     @PostMapping("/registerForJobSeeker")
     Result registerForJobSeeker(@Valid @RequestBody JobSeekerForRegisterDto jobSeekerForRegisterDto) {
 
-        var result = this.authService.registerForJobSeeker(jobSeekerForRegisterDto);
-
-        if (!result.isSuccess()) {
-            return new ErrorResult(Messages.FailedToAdd);//BURADA KALDIK!
-        }
-        return result;
+        return this.authService.registerForJobSeeker(jobSeekerForRegisterDto);
     }
 
     @PostMapping("/registerForEmployer")
     public Result registerForEmployer(@Valid @RequestBody EmployerForRegisterDto employerForRegisterDto) {
-        var result = this.authService.registerForEmployer(employerForRegisterDto);
 
-        if (!result.isSuccess()) {
-            return new ErrorResult(Messages.FailedToAdd);
-        }
-        return result;
+        return this.authService.registerForEmployer(employerForRegisterDto);
     }
 
     @PostMapping("/loginForUser")
     Result loginForUser(@Valid @RequestBody UserForLoginDto userForLoginDto) {
-        var result = this.authService.loginForUser(userForLoginDto);
 
-        if (!result.isSuccess()) {
-            return new ErrorResult();
-        }
-        return result;
+        return this.authService.loginForUser(userForLoginDto);
     }
 }
