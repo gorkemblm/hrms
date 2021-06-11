f4a8905baf8512e52890e556bbb25c1b5591aa42
Gorkemblm <gorkemblm@gmail.com>
2021-06-11 16:30:02 +0300

    added configuration

---
+++ Hrms/src/main/java/com/gorkem/hrms/configuration/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.gorkem.hrms.configuration;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", "gorkemblm",
+            "api_key", "194311365163593",
+            "api_secret", "ryNDRQ_c4dCW_25f20kI-lbtV6w"));
+}
