42cc65eec8a5dd134285e0b678502fd9f1ef27cf
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-06-15 19:38:03 +0300

    Work type/ work time type (#24)
    * added working time entity
    
    * added way of working entity

---
+++ Hrms/src/main/java/com/gorkem/hrms/entities/concretes/WorkingTime.java
@@ -0,0 +1,27 @@
+package com.gorkem.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "working_time")
+public class WorkingTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "type",nullable = false,length = 50)
+    private String type;
+
+    @OneToMany(mappedBy = "workingTime")
+    private List<JobAdvertisement> jobAdvertisements;
+}
