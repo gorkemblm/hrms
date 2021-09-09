611011b913478d4430adc769cd2c33e278556c02
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-09 18:36:30 +0300

    code refactor

--- Hrms/src/main/java/com/gorkem/hrms/business/constants/Messages.java
+++ Hrms/src/main/java/com/gorkem/hrms/business/constants/Messages.java
@@ -2,41 +2,43 @@ package com.gorkem.hrms.business.constants;
 
 public class Messages {
 
-    public static String successfullyAdded = "Successfully added.";
+    public static String ADD_SUCCESSFUL = "Successfully added.";
 
-    public static String FailedToAdd  = "Adding failed.";
+    public static String ADD_FAILED = "Adding failed.";
 
-    public static String FailedToList  = "Listing failed.";
+    public static String LIST_FAILED = "Listing failed.";
 
-    public static String successfullyListed = "Successfully listed.";
+    public static String LIST_SUCCESSFUL = "Successfully listed.";
 
-    public static String existInRecords = "This record already exists.";
+    public static String EXIST_RECORD = "This record already exists.";
 
-    public static String requiredFields = "All fields are required, please check it.";
+    public static String REQUIRED_FIELDS = "All fields are required, please check it.";
 
-    public static String existInSystem = "Your information is registered in the system, please check it.";
+    public static String EXIST_IN_SYSTEM = "Your information is registered in the system, please check it.";
 
-    public static String notVerifyMail = "This mail is not verified, please check it.";
+    public static String MAIL_NOT_VERIFY = "This mail is not verified, please check it.";
 
-    public static String sendToVerificationMail = "Confirmation email has been sent. Please check your inbox";
+    public static String SEND_TO_VERIFICATION_MAIL = "Confirmation email has been sent. Please check your inbox";
 
-    public static String registerSuccessfully = "You have successfully registered";
+    public static String REGISTER_SUCCESSFUL = "You have successfully registered";
 
-    public static String registerFailure = "Sorry, your registration failed";
+    public static String REGISTER_FAILED = "Sorry, your registration failed";
 
-    public static String loginSuccessfully = "You have successfully logged in";
+    public static String LOGIN_SUCCESSFUL = "You have successfully logged in";
 
-    public static String loginFailure = "You have encountered an error while logging in.";
+    public static String LOGIN_FAILED = "You have encountered an error while logging in.";
 
-    public static String successfullyFind = "Successfully brought";
+    public static String FIND_SUCCESSFUL = "Successfully brought";
+
+    public static String FIND_FAILED = "The requested data was not found";
 
     //Job Seeker
-    public static String notExistGovernment = "Your information is not registered in the government system.";
+    public static String NOT_PERSON = "Your information is not registered in the government system.";
 
     //Employer
-    public static String wrongMailFormat = "The e-mail format you entered is incorrect, please check it.";
+    public static String WRONG_MAIL_FORMAT = "The e-mail format you entered is incorrect, please check it.";
 
-    public static String updateJobAdvertisementSuccess = "The status of your job posting has been successfully updated.";
+    public static String UPDATE_JOB_ADVERTISEMENT_SUCCESSFUL = "The status of your job posting has been successfully updated.";
 
-    public static String updateJobAdvertisementFail = "Could not update the status of your job posting.";
+    public static String UPDATE_JOB_ADVERTISEMENT_FAILED = "Could not update the status of your job posting.";
 }
